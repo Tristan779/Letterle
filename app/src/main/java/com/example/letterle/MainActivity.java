a8c46e06a30d9fcda3b2d40ad031c67f2c029940
LAPTOP-SBHDQDIL\Tristan <tristan.pelgrims@gmail.com>
2022-04-23 12:07:55 +0200

    De letter A werkt, mijn hele uitleg staat in Messenger

--- app/src/main/java/com/example/letterle/MainActivity.java
+++ app/src/main/java/com/example/letterle/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.letterle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.CharacterIterator;
@@ -24,7 +26,16 @@ public class MainActivity extends AppCompatActivity {
         Collections.addAll(possibleWords, "anker", "kwaad", "speld", "steel", "loper");
     }
 
-    public int onBtnEnter_Clicked(String wordEntered)
+    public void onBtnLetter_Clicked(View caller)
+    {
+        Button letterButton = (Button) findViewById(R.id.buttonA);
+        String buttonText = letterButton.getText().toString();
+        TextView textViewNow = (TextView) findViewById(R.id.textViewLetter0);
+        textViewNow.setText(buttonText);
+    }
+
+
+    public int onBtnEnter_Clicked(View caller)
     {
         for(int count = 0; count < word.length(); count++) {
             String letter = String.valueOf(word.charAt(count));
@@ -32,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
             while (iterator.current() != CharacterIterator.DONE) {
                 if (letter.equals(iterator.current())) {
                     TextView textViewNow = (TextView) findViewById(R.id.textViewLetter0); //moet natuurlijk altijd de juiste zijn (iterator.getindex() ipv gwn 1
-                    textViewNow.setText(letter);
                     iterator.next();
                 }
             }
